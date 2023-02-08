f777388eaeae0d4c317072e5f4277ebab40f16a6
DylanDuignan <s00222859@atu.ie>
2023-02-08 11:08:42 +0000

    Created the Movie Service & movd array of movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'movie-list';
 
-  movieList = [
-    { id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola' },
-    { id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'},
-    { id: 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'},
-    { id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
-  ];
+  movies=[] as any;
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+    this.movies = this.movieService.getMovies();
+    console.log(this.movies);
+  }
 }
